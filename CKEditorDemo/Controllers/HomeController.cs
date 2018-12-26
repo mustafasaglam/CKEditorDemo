1d5520936b3e44c8132cf9b6d7d3ac6746bf7c5b
Mustafa.Saglam <Mustafa.Saglam@KSN70002179>
2018-12-26 14:18:25 +0300

    Add project files.

---
+++ CKEditorDemo/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using CKEditorDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CKEditorDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        MakaleDBEntities db = new MakaleDBEntities();
+        // GET: Home
+        public ActionResult Index()
+        {
+            var makale = db.Makales.ToList();
+            return View(makale);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateInput(false)]
+        public ActionResult Create(Makale m)
+        {
+          
+            db.Makales.Add(m);
+            db.SaveChanges();
+            return RedirectToAction("Create", "Home");
+        }
+
+    }
+}
